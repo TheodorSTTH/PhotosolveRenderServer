813ca95596a404f07d4c1386577a56ed912f69f0
Theodor <95082046+TheodorSTTH@users.noreply.github.com>
2023-02-12 02:12:58 +0100

    switched to get func

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ const Tesseract = require('tesseract.js');
 
 const app = express();
 const port = 3000;
-app.post('/', async function (req, res) {
-    // const { url } = req.body;
+// app.post('/', async function (req, res) {
+//     // const { url } = req.body;
+//     let out = "NOTEXT"
+//     await Tesseract.recognize(
+//         'https://tesseract.projectnaptha.com/img/eng_bw.png',
+//         'eng'
+//         // { logger: m => console.log(m) }
+//     ).then(({ data: { text } }) => {
+//         out = text;
+//     })
+//     res.send(out)
+// })
+app.get('/urlToText', async (req, res) => {  // url: /path?name='test'
+    const url = req.query['url'];  // result: test
     let out = "NOTEXT"
     await Tesseract.recognize(
-        'https://tesseract.projectnaptha.com/img/eng_bw.png',
+        url,
         'eng'
         // { logger: m => console.log(m) }
     ).then(({ data: { text } }) => {
         out = text;
     })
     res.send(out)
-})
-app.get('/', async function (req, res) {
-    res.send("The server is active on Render")
-})
+});
 console.log("Listening on port " + port)
 app.listen(port)
