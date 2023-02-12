33d035eab865952275a3c09755c47f28e0bfcf0b
Theodor <95082046+TheodorSTTH@users.noreply.github.com>
2023-02-12 02:25:55 +0100

    fix

--- index.js
+++ index.js
@@ -3,29 +3,34 @@ const Tesseract = require('tesseract.js');
 
 const app = express();
 const port = 3000;
-// app.post('/', async function (req, res) {
-//     // const { url } = req.body;
-//     let out = "NOTEXT"
-//     await Tesseract.recognize(
-//         'https://tesseract.projectnaptha.com/img/eng_bw.png',
-//         'eng'
-//         // { logger: m => console.log(m) }
-//     ).then(({ data: { text } }) => {
-//         out = text;
-//     })
-//     res.send(out)
-// })
-app.get('/urlToText', async (req, res) => {  // url: /path?name='test'
-    const url = req.query['url'];  // result: test
+app.post('/', async function (req, res) {
+    // const { url } = req.body;
     let out = "NOTEXT"
     await Tesseract.recognize(
-        url,
+        'https://tesseract.projectnaptha.com/img/eng_bw.png',
         'eng'
         // { logger: m => console.log(m) }
     ).then(({ data: { text } }) => {
         out = text;
     })
     res.send(out)
+})
+app.get('/urlToText', async (req, res) => {  // url: /path?name='test'
+    try {
+        const url = req.query['url'];  // result: test
+        let out = "ERR_NOTEXT"
+        await Tesseract.recognize(
+            url,
+            'eng'
+            // { logger: m => console.log(m) }
+        ).then(({ data: { text } }) => {
+            out = text;
+        })
+        res.send(out)
+    }
+    catch {
+        res.send("ERR_CATCH")
+    }
 });
 console.log("Listening on port " + port)
 app.listen(port)
